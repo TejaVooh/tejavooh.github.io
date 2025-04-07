ea2e3586e84ebc918fa7f3e05ec856caf5fd5c97
TejaVooh <tejanshu@gmail.com>
2025-04-07 20:21:41 +0530

    php errors fix attempt 1

--- visitor.js
+++ visitor.js
@@ -1,7 +1,7 @@
 fetch('https://ipapi.co/json')
   .then(res => res.json())
   .then(data => {
-    fetch('https://tejanshu.com/logger/visitor_log.php', {
+    fetch('https://www.tejanshu.com/logger/visitor_log.php', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -17,7 +17,7 @@ fetch('https://ipapi.co/json')
   .catch(err => console.error("Visitor log failed:", err));
 
   function updateConkyStats() {
-    fetch('https://tejanshu.com/logger/read_logs.php')
+    fetch('https://www.tejanshu.com/logger/read_logs.php')
       .then(res => res.json())
       .then(logs => {
         const panel = document.getElementById('conkyStats');
